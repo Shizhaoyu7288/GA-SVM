cbdf266e70b3035269f58f0d883b8069b8d36c71
Shizhaoyu7288 <75065806+Shizhaoyu7288@users.noreply.github.com>
2022-03-16 17:09:48 +0800

    Delete test1.m

--- test1.m
+++
@@ -1,7 +0,0 @@
-% model = svmtrain(heart_scale_label, heart_scale_inst, '-c 1 -g 0.07');
-% [predict_label, accuracy, dec_values] = svmpredict(heart_scale_label, heart_scale_inst, model);
-% template.x = [];
-% template.y = [];
-model = svmtrain(data_label,data_train , '-c 1 -g 0.07');
-[predict_label, accuracy, dec_values] = svmpredict(data_label,data_train , model);
-CR = ClassResult(data_label,data_train,model);
